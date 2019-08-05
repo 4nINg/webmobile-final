77226a2938c48438bb9960445c1bca8325b593d4
nnyong <jo.youthful@gmail.com>
2019-08-05 17:24:23 +0900

    login & signup form

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -1,60 +1,75 @@
 <template>
-<div id="loginForm">
-  <div><span>E-Mail</span><input type="text" placeholder="ID" id="loginID" v-model="email" required></div>
-  <div><span>Password</span><input type="password" placeholder="Password" id="loginPW" v-model="password" required></div>
-  <button @click="userSignIn()">SignIn</button>
-  <button @click="signUp()">SignUp</button>
-  <button @click="loginWithGoogle()">Google</button>
-  <button @click="loginWithFacebook()">Facebook</button>
-</div>
+  <div class="loginMainDiv">
+    <div class="loginbox">
+      <span class="avatar">
+        <i class="far fa-user-circle"></i>
+      </span>
+      <h1 class="loginFormTitle">Login</h1>
+      <div class="loginFormInputArray">
+        <p>Email</p>
+        <input type="text" placeholder="Email" />
+        <p>Password</p>
+        <input type="password" placeholder="Password" />
+      </div>
+      <div class="loginFormButton">
+        <span @click="userSignIn()">Login</span>
+        <span @click="signUp()">Signup</span>
+        <span @click="loginWithGoogle()" class="Google">
+          <i class="fab fa-google"></i>&nbsp;Google
+        </span>
+        <span @click="loginWithFacebook()" class="Facebook">
+          <i class="fab fa-facebook-f"></i>&nbsp;Facebook
+        </span>
+      </div>
+    </div>
+  </div>
 </template>
 <script>
-import SignUpForm from "@/components/SignUpForm"
+import SignUpForm from "@/components/SignUpForm";
 
 export default {
   name: "LoginForm",
-  components:{
+  components: {
     SignUpForm
   },
-  data(){
-    return{
+  data() {
+    return {
       email: "",
       password: ""
-    }
+    };
   },
-  methods : {
-    userSignIn(){
-      this.$store.dispatch("userSignIn", {
-        email: this.email,
-        password: this.password
-      })
-      .finally(()=>{
-        this.changeSelectPage(0);
-        // if(!this.$store.getters.isAdmin){
-        //   document.querySelector("#adminPageBtn").style.display = "none";
-        // }else{
-        //   document.querySelector("#adminPageBtn").style.display = "block";
-        // }
-      });
+  methods: {
+    userSignIn() {
+      this.$store
+        .dispatch("userSignIn", {
+          email: this.email,
+          password: this.password
+        })
+        .finally(() => {
+          this.changeSelectPage(0);
+          // if(!this.$store.getters.isAdmin){
+          //   document.querySelector("#adminPageBtn").style.display = "none";
+          // }else{
+          //   document.querySelector("#adminPageBtn").style.display = "block";
+          // }
+        });
     },
     //google login
     loginWithGoogle() {
-      this.$store.dispatch("userSignInWithGoogle", {})
-      .finally(()=>{
+      this.$store.dispatch("userSignInWithGoogle", {}).finally(() => {
         this.changeSelectPage(0);
       });
     },
     //facebook login
     loginWithFacebook() {
-      this.$store.dispatch("userSignInWithFacebook", {})
-      .finally(()=>{
+      this.$store.dispatch("userSignInWithFacebook", {}).finally(() => {
         this.changeSelectPage(0);
       });
     },
-    signUp(){
+    signUp() {
       this.changeSelectPage(5);
     },
-    changeSelectPage(i){
+    changeSelectPage(i) {
       this.$emit("inLoginSelectPage", i);
     }
   },
@@ -78,18 +93,123 @@ export default {
       }
     }
   }
-}
+};
 </script>
 <style>
-
-#loginForm{
-  /* margin-left: 30%;
-  margin-top: 30%; */
-  position: relative;
-  top:25%;
-  left:40%;
+.loginMainDiv {
+  /* position: relative; */
+  width: 90%;
+  height: 81%;
+  margin-left: 10%;
+  display: flex;
+  align-items: center;
+  /* margin-top: 0 !important; */
 }
-#loginForm button{
-  margin-right:5px;
+.loginbox {
+  width: 30%;
+  height: 70%;
+  margin-bottom: 3%;
+  background-color: rgb(255, 255, 255, 0.8);
+  margin-left: 30%;
+  box-sizing: border-box;
+  /* border-box: 테두리 기준으로 크기 정함. */
+  font-family: sans-serif;
+  /* border: 1px solid black; */
+  box-shadow: 1px 1px 1px 1px rgb(212, 212, 212);
+  letter-spacing: 0.05em;
+  position: relative;
+}
+.avatar {
+  /* width: 20%;
+  height: 20%; */
+  font-size: 5em;
+  color: rgb(0, 0, 0, 0.5);
+  border-radius: 30%;
+  /* position: absolute; */
+  margin-top: -6%;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  /* background-color: rgb(255, 255, 255, 0.6); */
+  /* background-color: black; */
+  position: absolute;
+  top: -5%;
+}
+.loginFormInputArray {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+}
+
+.loginFormInputArray p {
+  margin-top: 1em;
+  width: 80%;
+  font-weight: bold;
+  justify-content: flex-start;
+}
+.loginFormInputArray input {
+  width: 80%;
+  border: none;
+  border-bottom: 1px solid #000;
+  background: transparent;
+  outline: none;
+  height: 2em;
+}
+.loginFormTitle {
+  text-align: center;
+  font-size: 22px;
+  margin-top: 13%;
+}
+.loginFormButton {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-top: 4%;
+}
+
+.loginFormButton span {
+  font-family: sans-serif;
+  text-align: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 0.8em;
+  width: 80%;
+  border: none;
+  outline: none;
+  border: 1px solid rgb(184, 184, 184);
+  background: transparent;
+  height: 1.7em;
+  font-size: 0.8em;
+  font-weight: bold;
+  box-shadow: 1px 1px 1px 1px rgb(119, 119, 119);
+  /* text-transform: uppercase; */
+}
+.fa-google {
+  color: rgb(207, 61, 48);
+}
+.fa-facebook-f {
+  color: rgb(46, 72, 122);
+}
+.loginFormButton span:nth-child(1):hover,
+.loginFormButton span:nth-child(2):hover {
+  background-color: black;
+  color: white;
+  transition: 1s;
+}
+.Google:hover {
+  background-color: rgb(234, 67, 53);
+  color: white;
+  border: 1px solid rgb(234, 67, 53);
+  transition: 1s;
+}
+.Facebook:hover {
+  background-color: rgb(66, 103, 178);
+  color: white;
+  border: 1px solid rgb(66, 103, 178);
+  transition: 1s;
 }
 </style>
--- src/components/SignUpForm.vue
+++ src/components/SignUpForm.vue
@@ -1,11 +1,33 @@
 <template>
-<div id="signUpForm">
-    <div><span>E-Mail</span><input type="text" placeholder="ID" id="signUpID" v-model="email" required></div>
-    <div><span>Password</span><input type="password" placeholder="Password" id="signUpPW" v-model="password" required></div>
-    <div><span>Check Password</span><input type="password" placeholder="Check Password" id="signUpCheckPW" v-model="passwordCheck" :rules="[comparePasswords]" required></div>
-    <div><span>Username</span><input type="text" placeholder="Username" id="signUpUsername" v-model="username" required></div>
-    <button @click="userSignUp()">회원가입</button><button @click="userSignUpCancle()">취소</button>
-</div>
+  <div class="SignupMainDiv">
+    <div class="signupbox">
+      <span class="avatar">
+        <!-- <i class="fas fa-user-plus"></i> -->
+      </span>
+      <h1 class="signupFormTitle">Sign Up</h1>
+      <div class="signupFormInputArray">
+        <p>Email</p>
+        <input type="text" placeholder="Email" id="signUpID" v-model="email" required />
+        <p>Username</p>
+        <input type="text" placeholder="Username" id="signUpUsername" v-model="username" required />
+        <p>Password</p>
+        <input type="password" placeholder="Password" id="signUpPW" v-model="password" required />
+        <p>Password Confirm</p>
+        <input
+          type="password"
+          placeholder="Password Confirm"
+          id="signUpCheckPW"
+          v-model="passwordCheck"
+          :rules="[comparePasswords]"
+          required
+        />
+      </div>
+      <div class="signupFormButton">
+        <span @click="userSignUp()">회원가입</span>
+        <span @click="userSignUpCancle()">취소</span>
+      </div>
+    </div>
+  </div>
 </template>
 <script>
 export default {
@@ -18,13 +40,13 @@ export default {
       passwordCheck: "",
       alert: false,
       dialog: false
-    }
+    };
   },
   computed: {
     comparePasswords() {
-      return this.password === this.passwordCheck ?
-        true :
-        "Passwords don't match";
+      return this.password === this.passwordCheck
+        ? true
+        : "Passwords don't match";
     },
     error() {
       return this.$store.state.error;
@@ -34,29 +56,31 @@ export default {
     }
   },
   methods: {
-    props:[
-      'changeSelectPage'
-    ],
-    userSignUp() { //회원가입
+    props: ["changeSelectPage"],
+    userSignUp() {
+      //회원가입
       if (this.comparePasswords !== true) {
         return;
       }
-      this.$store.dispatch("userSignUp", {
-        email: this.email,
-        username: this.username,
-        password: this.password
-      }).finally(()=>{
-        this.changeSelectPage(0);
-      })
+      this.$store
+        .dispatch("userSignUp", {
+          email: this.email,
+          username: this.username,
+          password: this.password
+        })
+        .finally(() => {
+          this.changeSelectPage(0);
+        });
     },
-    userSignUpCancle() { //회원가입 취소
+    userSignUpCancle() {
+      //회원가입 취소
       this.email = "";
       this.password = "";
       this.passwordCheck = "";
       this.username = "";
       this.changeSelectPage(4);
     },
-    changeSelectPage(i){
+    changeSelectPage(i) {
       this.$emit("inSignUpSelectPage", i);
     }
   },
@@ -72,20 +96,91 @@ export default {
       }
     }
   }
-}
+};
 </script>
 <style>
-
-#signUpForm{
-  /* margin-left: 30%;
-  margin-top: 30%; */
+.SignupMainDiv {
+  /* position: relative; */
+  width: 90%;
+  height: 81%;
+  margin-top: -2%;
+  margin-left: 10%;
+  display: flex;
+  align-items: center;
+}
+.signupbox {
+  width: 30%;
+  height: 76%;
+  /* margin-bottom: 3%; */
+  background-color: rgb(255, 255, 255, 0.8);
+  margin-left: 30%;
+  box-sizing: border-box;
+  /* border-box: 테두리 기준으로 크기 정함. */
+  font-family: sans-serif;
+  box-shadow: 1px 1px 1px 1px rgb(212, 212, 212);
+  letter-spacing: 0.05em;
   position: relative;
-  top:25%;
-  left:40%;
+}
+.signupFormInputArray {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
 }
 
-#signUpForm button{
-  margin-right:5px;
+.signupFormInputArray p {
+  margin-top: 1em;
+  width: 80%;
+  font-weight: bold;
+  justify-content: flex-start;
+}
+.signupFormInputArray input {
+  width: 80%;
+  border: none;
+  border-bottom: 1px solid #000;
+  background: transparent;
+  outline: none;
+  height: 2em;
+}
+.signupFormTitle {
+  text-align: center;
+  font-size: 22px;
+  margin-top: 5%;
+  margin-bottom: 2%;
+}
+.signupFormButton {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-top: 4%;
 }
 
+.signupFormButton span {
+  font-family: sans-serif;
+  text-align: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 0.8em;
+  width: 80%;
+  border: none;
+  outline: none;
+  border: 1px solid rgb(184, 184, 184);
+  background: transparent;
+  height: 1.7em;
+  font-size: 0.8em;
+  font-weight: bold;
+  box-shadow: 1px 1px 1px 1px rgb(119, 119, 119);
+}
+.signupFormButton span:nth-child(1):hover,
+.signupFormButton span:nth-child(2):hover {
+  background-color: black;
+  color: white;
+  transition: 1s;
+}
+
+.signupFormButton span:nth-child(2) {
+  margin-top: 4%;
+}
 </style>
